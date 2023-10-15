b2738f09f54276af7723a6932fd89dfb3080d7a5
hkthc10 <buithaithinh69@gmail.com>
2023-10-15 12:03:50 +0700

    new commit

---
+++ b1/b1.js
@@ -0,0 +1,50 @@
+// string:tập hợp của nhiều chữ cái số ở trong ngoặc kép
+//    vd:abc123
+// number:chuôix số
+// vd:123....,.....
+
+
+
+
+// dấu căn bậc 2 : Math.sqrt(value) , Math.sqrt(100) = 10
+// let count = a ** b;
+
+
+
+// let a = 3;
+// let b = 4;
+// let c = 5;
+// let p = (a + b + c) / 2;
+
+// let t = p * (p - a) * (p - b) * (p - c);
+// let s = Math.sqrt(t);
+
+// console.log("Diện tích tam khi có số đo 3 cạnh lần lượt là "+ a + ", "+ b + ", "+ c+ " = "+ s);
+// console.log(`Diện tích tam khi có số đo 3 cạnh lần lượt là ${a}, ${b}, ${c} = ${s}`);
+
+
+// hàm alert,Prompt,ép biểu trong js:
+// string(value):biến value tf kiểu giá trị nào đó --> kiểu giá trị string
+// number(value):biến value tf kiểu giá trị nào đó --> kiểu giá trị number
+// alert
+// let input_value1 = prompt("Enter a ?");
+// input_value1 = Number(input_value1)
+// let a = input_value1
+
+// let input_value2 = prompt("Enter b ?");
+// input_value2 = Number(input_value2)
+// let b = input_value2
+
+// let input_value3 = prompt("Enter c ?");
+// input_value3 = Number(input_value3)
+// let c = input_value3
+// let p = (a + b + c) / 2;
+// let t = p * (p - a) * (p - b) * (p - c)
+// console.log(t)
+//   các cặp đk trong js
+// &&:và
+// ||:hoặc
+let a = 10
+let b = "10"
+let check = a === b
+console.log(check); 
